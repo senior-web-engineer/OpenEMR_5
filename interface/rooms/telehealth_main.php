fac25d37a880577753779049aba0b374c04f48bc
khoa-ng <khoa-ng112@outlook.com>
2020-10-16 16:34:51 +0800

    add webrtc

--- interface/rooms/telehealth_main.php
+++ interface/rooms/telehealth_main.php
@@ -94,9 +94,265 @@ if (isset($_REQUEST["mode"])) {
     <head>
         <title><?php echo xlt('Rooms / Telehealth');?></title>
         <?php Header::setupHeader(['common','jquery-ui']); ?>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.js"></script>
+        <script src="https://webrtc.github.io/adapter/adapter-latest.js"></script>
         <script type="text/javascript">
+            var isChannelReady = false;
+            var isInitiator = false;
+            var isStarted = false;
+            var localStream;
+            var pc;
+            var remoteStream;
+            var turnReady;
+
+            var pcConfig = {
+                'iceServers': [{
+                    'urls': 'stun.stun.l.google.com:19302'
+                }]
+            };
+
+            var sdpConstraints = {
+                offerToReceiveAudio: true,
+                offerToReceiveVideo: true
+            };
+
+            var room = 'foo';
+
+            var socket = io.connect();
+
+            if (room !== '') {
+                socket.emit('create or join', room);
+                console.log('Attpemted to create or join room', room);
+            }
+
+            socket.on('created', function (room) {
+                console.log('Created room ' + room);
+                isInitiator = true;
+            });
+
+            socket.on('full', function (room) {
+                console.log('Room ' + room + ' is full');
+            });
+
+            socket.on('join', function (room) {
+                console.log('Another peer made a request to join room ' + room);
+                console.log('This peer is the initiator of room ' + room + '!');
+                isChannelReady = true;
+            });
+
+            socket.on('joined', function (room) {
+                console.log('joined: ' + room);
+                isChannelReady = true;
+            });
+
+            socket.on('log', function (array) {
+                console.log.apply(console, array);
+            });
+
+            function sendMessage(message) {
+                console.log('Client sending message: ', message);
+                socket.emit('message', message);
+            }
+
+            // This client receives a message
+            socket.on('message', function (message) {
+                console.log('Client received message:', message);
+                if (message === 'got user media') {
+                    maybeStart();
+                } else if (message.type === 'offer') {
+                    if (!isInitiator && !isStarted) {
+                        maybeStart();
+                    }
+                    pc.setRemoteDescription(new RTCSessionDescription(message));
+                    doAnswer();
+                } else if (message.type === 'answer' && isStarted) {
+                    pc.setRemoteDescription(new RTCSessionDescription(message));
+                } else if (message.type === 'candidate' && isStarted) {
+                    var candidate = new RTCIceCandidate({
+                        sdpMLineIndex: message.label,
+                        candidate: message.candidate
+                    });
+                    pc.addIceCandidate(candidate);
+                } else if (message === 'bye' && isStarted) {
+                    handleRemoteHangup();
+                }
+            });
+
+            var localVideo = document.querySelector('#localVideo');
+            var remoteVideo = document.querySelector('#remoteVideo');
+
+            // navigator.mediaDevices.getUserMedia({
+            //     audio: false,
+            //     video: true
+            // })
+            // .then(gotStream)
+            // .catch(function (e) {
+            //     alert('getUserMedia() error: ' + e.name);
+            // });
+
+            function gotStream(stream) {
+                console.log('Adding local stream.');
+                localStream = stream;
+                localVideo.srcObject = stream;
+                sendMessage('got user media');
+                if (isInitiator) {
+                    maybeStart();
+                }
+            }
+
+            var constraints = {
+                video: true
+            };
+
+            console.log('Getting user media with constraints', constraints);
+
+            if (location.hostname !== 'localhost') {
+                requestTurn('https://computeengineondemand.appspot.com/turn?username=41784574&key=4080218913');
+            }
+
+            function maybeStart() {
+                console.log('>>>>>>>>> maybeStart() ', isStarted, localStream, isChannelReady);
+                if (!isStarted && typeof localStream !== 'undefined' && isChannelReady) {
+                    console.log('>>>>>>>>> creating peer connection');
+                    createPeerConnection();
+                    pc.addStream(localStream);
+                    isStarted = true;
+                    console.log('isInitiator', isInitiator);
+                    if (isInitiator) {
+                        doCall();
+                    }
+                }
+            }
+
+            window.onbeforeunload = function () {
+                sendMessage('bye');
+            }
+
+            function createPeerConnection() {
+                try {
+                    pc = new RTCPeerConnection(null);
+                    pc.onicecandidate = handleIceCandidate;
+                    pc.onaddstream = handleRemoteStreamAdded;
+                    pc.onremovestream = handleRemoteStreamRemoved;
+                    console.log('Created RTCPeerConnection');
+                } catch (e) {
+                    console.log('Failed to create PeerConnection , exception: ' + e.message);
+                    alert('Cannot create RTCPeerConnection object.');
+                    return;
+                }
+            }
+
+            function handleIceCandidate(event) {
+                console.log('icecandidate event: ', event);
+                if (event.candidate) {
+                    sendMessage({
+                        type: 'candidate',
+                        label: event.candidate.sdpMLineIndex,
+                        id: event.candidate.sdpMid,
+                        candidate: event.candidate.candidate
+                    });
+                } else {
+                    console.log('End of candidates');
+                }
+            }
+
+            function handleCreateOfferError(event) {
+                console.log('createOffer() error: ', event);
+            }
+
+            function doCall() {
+                console.log('Sending offer to peer');
+                pc.createOffer(setLocalAndSendMessage, handleCreateOfferError);
+            }
+
+            function doAnswer() {
+                console.log('Sending answer to peer.');
+                pc.createAnswer().then(
+                    setLocalAndSendMessage,
+                    onCreateSessionDescriptionError
+                );
+            }
+
+            function setLocalAndSendMessage(sessionDescription) {
+                pc.setLocalDescription(sessionDescription);
+                console.log('setLocalAndSendMessage sending message', sessionDescription);
+                sendMessage(sessionDescription);
+            }
+
+            function onCreateSessionDescriptionError(error) {
+                console.trace('Failed to create session description: ' + error.toString());
+            }
+
+            function requestTurn(turnURL) {
+                var turnExists = false;
+
+                for (var i in pcConfig.iceServers) {
+                    if (pcConfig.iceServers[i].urls.substr(0, 5) === 'turn:') {
+                        turnExists = true;
+                        turnReady = true;
+                        break;
+                    }
+                }
+                if (!turnExists) {
+                    console.log('Getting TURN server from ', turnURL);
+                    // No TURN server. Get one from computengineondemand.appspot.com;
+                    var xhr = new XMLHttpRequest();
+                    xhr.onreadystatechange = function () {
+                        if (xhr.readyState === 4 && xhr.status === 200) {
+                            var turnServer = JSON.parse(xhr.responseText);
+                            console.log('Got TURN server: ', turnServer);
+                            pcConfig.iceServers.push({
+                                'urls': 'turn:' + turnServer.username + '@' + turnServer.turn,
+                                'credential': turnServer.password
+                            });
+                            turnReady = true;
+                        }
+                    };
+                    xhr.open('GET', turnURL, true);
+                    xhr.send();
+                }
+            }
+
+            function handleRemoteStreamAdded(event) {
+                console.log('Remote stream added.');
+                remoteStream = event.stream;
+                remoteVideo.srcObject = remoteStream;
+            }
+
+            function handleRemoteStreamRemoved(event) {
+                console.log('Remote stream removed. Event: ', event);
+            }
+
+            function hangup() {
+                console.log('Hanging up');
+                stop();
+                sendMessage('bye');
+            }
+
+            function handleRemoteHangup() {
+                console.log('Session terminated.');
+                stop();
+                isInitiator = false;
+            }
+
+            function stop() {
+                isStarted = false;
+                pc.close();
+                pc = null;
+            }
+
             function start_session(room_link) {
                 location.href = room_link;
+
+                navigator.mediaDevices.getUserMedia({
+                    audio: false,
+                    video: true
+                })
+                .then(gotStream)
+                .catch(function (e) {
+                    alert('getUserMedia() error: ' + e.name);
+                });
             }
 
             function change_room() {
@@ -169,8 +425,12 @@ if (isset($_REQUEST["mode"])) {
                                 <?php if ($meetingurl == '') { ?>
                                     <img src='./img/avatar.png' width='480px' height='480px'>
                                 <?php } else { ?>
-                                    <div class="iframe-container" style="overflow: hidden; width:480px; height:480px; position: relative;">
-                                        <iframe allow="microphone; camera" style="border: 0; height: 100%; left: 0; position: absolute; top: 0; width: 100%;" src="<?php echo $meetingurl; ?>" sandbox="allow-forms allow-scripts allow-same-origin" allow="microphone; camera"></iframe>
+<!--                                    <div class="iframe-container" style="overflow: hidden; width:480px; height:480px; position: relative;">-->
+<!--                                        <iframe allow="microphone; camera" style="border: 0; height: 100%; left: 0; position: absolute; top: 0; width: 100%;" src="--><?php //echo $meetingurl; ?><!--" sandbox="allow-forms allow-scripts allow-same-origin" allow="microphone; camera"></iframe>-->
+<!--                                    </div>-->
+                                    <div id="videos" style="overflow: hidden; width: 480px; height: 480px; position: relative;">
+                                        <video id="localVideo" style="display: none" autoplay muted playsinline></video>
+                                        <video id="remoteVideo" autoplay playsinline></video>
                                     </div>
                                 <?php } ?>
                             </td>
